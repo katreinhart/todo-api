f10c3cf11b368d82077f49d147a84a6a36e8b824
Kat Reinhart <kat.reinhart@gmail.com>
2016-01-16 09:50:33 -0800

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet Mom for lunch',
-	completed: false
-}, {
-	id: 2, 
-	description: 'Go to store',
-	completed: false
-}, {
-	id: 3,
-	description: 'Come home from work',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -27,10 +20,10 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res){
 	var todoId = req.params.id;
 	var matchedTodo;
-	// iterate over todos array and find match
+//	iterate over todos array and find match
 	
 	for(var i=0; i<todos.length; i++){
-		if(todos[i].id == todoId){ // if this is a ===, it doesn't work. hmm.
+		if(todos[i].id == todoId){ // use this instead of parseInt? 
 			matchedTodo = todos[i];
 			res.json(matchedTodo); //is it bad to call this inside the iterator?
 		}
@@ -40,10 +33,22 @@ app.get('/todos/:id', function (req, res){
 		 res.status(404).send(); // if there is no match send a 404
 	}
 	
-	//res.send('Asking for todo with id of ' + req.params.id);
+//	res.send('Asking for todo with id of ' + req.params.id);
 	
 	
 } );
+
+// POST /todos
+app.post( '/todos', function( req, res ) {
+	var body = req.body;
+	body.id = todoNextId++;
+	
+	todos.push(body);
+
+	res.json(body);
+	
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '.');
