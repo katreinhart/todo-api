d1c089313910835ba2c7f1591bf072cbf5bb6b1c
Kat Reinhart <kat.reinhart@gmail.com>
2016-01-16 10:57:19 -0800

    Added a DELETE route

--- server.js
+++ server.js
@@ -19,15 +19,8 @@ app.get('/todos', function (req, res) {
 
 
 app.get('/todos/:id', function (req, res){
-	var todoId = parseInt(req.params.id);
+	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
-
-// 	for(var i=0; i<todos.length; i++){
-// 		if(todos[i].id == todoId){ // use this instead of parseInt? 
-// 			matchedTodo = todos[i];
-// 			res.json(matchedTodo); //is it bad to call this inside the iterator?
-// 		}
-// 	}
 	
 	if(matchedTodo){
 		res.json(matchedTodo).send();
@@ -35,17 +28,19 @@ app.get('/todos/:id', function (req, res){
 		res.status(404).send(); // if there is no match send a 404
 	}
 
-} );
+});
 
 // POST /todos
 app.post( '/todos', function( req, res ) {
 	var body = _.pick(req.body, 'description', 'completed');
 	
-	body.description = body.description.trim();
 	
-	if(! _.isBoolean(body.completed) || !_.isString(body.description) || body.description.length === 0){
+	
+	if(! _.isBoolean(body.completed) || !_.isString(body.description.trim()) || body.description.trim().length === 0){
 		return res.status(400).send();
 	}
+	
+	body.description = body.description.trim();
 	
 	body.id = todoNextId++;
 	
@@ -55,7 +50,34 @@ app.post( '/todos', function( req, res ) {
 	
 });
 
+// DELETE /todos/:id
+
+app.delete( '/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
+	if(!matchedTodo){
+		res.status(404).json("error: no todo found matching id");
+	} else {	
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '.');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
