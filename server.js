5a65b949f575a37f97ad88608ae73666331ae266
Kat Reinhart <kat.reinhart@gmail.com>
2016-01-18 19:09:21 -0800

    Add login route and authenticate class methods to user model

--- server.js
+++ server.js
@@ -124,36 +124,16 @@ app.post('/users', function(req, res){
 app.post('/users/login', function(req, res){
 	var body = _.pick(req.body, 'email', 'password');
 
-	//check to see if email exists and is string
-	if(typeof body.email !== 'string' || typeof body.password !== 'string'){
-		return res.status(400).send();
-	}
-
-// use 	db.user.findOne() to find user with email passed in
-// inside promise callback call res.json
-// if doesn't find user, send a 500
-//
-// Project.findOne({ where: {title: 'aProject'} }).then(function(project) {
-//   // project will be the first entry of the Projects table with the title 'aProject' || null
-// })
-
-	db.user.findOne({
-		where: {
-			email: body.email
-		}
-	}).then(function(user){
-		if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))){
-			return res.status(401).send();
-		}
-
+	db.user.authenticate(body).then( function (user){
 		res.json(user.toPublicJSON());
-	}, function(e){
-		res.status(500).send();
+	}, function (e) {
+		res.status(401).send();
 	});
+
 });
 
 db.sequelize.sync(
-//	{force: true}
+	{force: true}
 ).then(function(){
     app.listen(PORT, function () {
 	   console.log('Express listening on port ' + PORT + '.');
